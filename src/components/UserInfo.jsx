b487661e53c93d6587aed41ca2c845b7129c2a6c
Utku Can Yılmaz <utkucanyilmaz26@gmail.com>
2023-08-24 23:56:04 +0300

    Update UserInfo component

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -7,11 +7,12 @@ function UserInfo() {
       <a
         href={`https://www.twitch.tv/${user[0].display_name}`}
         className="text-base font-bold text-white hover:text-purple-300 transition-colors flex items-center justify-center gap-x-2"
+        target="_blank"
       >
         <img
           className="h-8 rounded-full border border-purple-500"
           src={user[0].profile_image_url}
-          alt=""
+          alt={`${user[0].display_name} Profile Image`}
         />
         {user[0].display_name}
       </a>
